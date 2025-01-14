f76a46303c0ace3e80f30f2fb177e28494dcbbe6
Nguyễn Minh Đức <148557640+nmd1406@users.noreply.github.com>
2025-01-14 11:23:34 +0700

    refactoring chat screen so every 2 users have their own chat room

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -1,34 +1,53 @@
 import 'package:chat_app/widgets/chat_messages.dart';
 import 'package:chat_app/widgets/new_message.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class ChatScreen extends StatelessWidget {
-  const ChatScreen({super.key});
+  final Map<String, dynamic> otherUserData;
+  final String otherUserId;
+
+  const ChatScreen({
+    super.key,
+    required this.otherUserData,
+    required this.otherUserId,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("FlutterChat"),
+        title: Text(
+          otherUserData["username"],
+          style: TextStyle(
+            color: Theme.of(context).colorScheme.primary,
+          ),
+        ),
         centerTitle: true,
+        elevation: 20,
+        backgroundColor: Theme.of(context).colorScheme.primaryContainer,
         actions: [
           IconButton(
-              onPressed: () async {
-                await FirebaseAuth.instance.signOut();
-              },
-              icon: Icon(
-                Icons.exit_to_app,
-                color: Theme.of(context).colorScheme.primary,
-              ))
+            onPressed: () async {},
+            icon: Icon(
+              Icons.info_outline_rounded,
+              color: Theme.of(context).colorScheme.primary,
+              size: 30,
+            ),
+          )
         ],
       ),
       body: Column(
         children: [
           Expanded(
-            child: ChatMessages(),
+            child: ChatMessages(
+              otherUserData: otherUserData,
+              otherUserId: otherUserId,
+            ),
           ),
-          NewMessage(),
+          NewMessage(
+            otherUserData: otherUserData,
+            otherUserId: otherUserId,
+          ),
         ],
       ),
     );
