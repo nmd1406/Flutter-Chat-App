987b63a236e80dcd958564b956fe830fc6c4aaa9
Nguyễn Minh Đức <148557640+nmd1406@users.noreply.github.com>
2025-01-14 11:27:16 +0700

    update

--- lib/services/user_service.dart
+++ lib/services/user_service.dart
@@ -6,4 +6,11 @@ class UserService {
   Stream<QuerySnapshot<Map<String, dynamic>>> getUsersStream() {
     return _firestore.collection("users").snapshots();
   }
+
+  Future<DocumentSnapshot<Map<String, dynamic>>> getUserData(
+      String userId) async {
+    final userData =
+        await FirebaseFirestore.instance.collection("users").doc(userId).get();
+    return userData;
+  }
 }
