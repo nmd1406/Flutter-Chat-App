9725e052ff4f8725ccf3b349dd94d4311559d5e1
Nguyễn Minh Đức <148557640+nmd1406@users.noreply.github.com>
2025-02-18 00:14:44 +0700

    finish search message

--- lib/screens/messages.dart
+++ lib/screens/messages.dart
@@ -27,6 +27,37 @@ class _MessagesScreenState extends State<MessagesScreen> {
     return Column(
       mainAxisSize: MainAxisSize.min,
       children: [
+        Padding(
+          padding: const EdgeInsets.all(14.0),
+          child: SearchBar(
+            controller: _searchController,
+            hintText: "Tìm kiếm",
+            onChanged: (value) {
+              setState(() {
+                query = value;
+              });
+            },
+            onSubmitted: (value) {
+              setState(() {
+                query = value;
+              });
+            },
+            onTapOutside: (event) {
+              FocusScope.of(context).unfocus();
+            },
+            leading: Icon(Icons.search),
+            trailing: [
+              IconButton(
+                onPressed: () {
+                  setState(() {
+                    _searchController.clear();
+                  });
+                },
+                icon: Icon(Icons.close_rounded),
+              ),
+            ],
+          ),
+        ),
         Expanded(
           child: StreamBuilder(
             stream: _chatService.getPrivateChatRooms(currentUserUid),
@@ -46,94 +77,55 @@ class _MessagesScreenState extends State<MessagesScreen> {
               final chatRooms =
                   snapshot.data!.docs.map((doc) => doc.data()).toList();
 
-              return Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(14.0),
-                    child: SearchBar(
-                      controller: _searchController,
-                      hintText: "Tìm kiếm",
-                      onChanged: (value) {
-                        setState(() {
-                          query = value;
-                        });
-                      },
-                      onSubmitted: (value) {
-                        setState(() {
-                          query = value;
-                        });
-                      },
-                      onTapOutside: (event) {
-                        FocusScope.of(context).unfocus();
-                      },
-                    ),
-                  ),
-                  const SizedBox(height: 20),
-                  ListView.builder(
-                    shrinkWrap: true,
-                    itemCount: chatRooms.length,
-                    itemBuilder: (context, index) {
-                      final chatRoomId = chatRooms[index]["chatRoomId"];
-                      final otherUserId = chatRoomId[0] == currentUserUid
-                          ? chatRoomId[1]
-                          : chatRoomId[0];
-                      final user = _userService.getUserData(otherUserId);
+              return ListView.builder(
+                shrinkWrap: true,
+                itemCount: chatRooms.length,
+                itemBuilder: (context, index) {
+                  final chatRoomId = chatRooms[index]["chatRoomId"];
+                  final otherUserId = chatRoomId[0] == currentUserUid
+                      ? chatRoomId[1]
+                      : chatRoomId[0];
+                  final user = _userService.getUserData(otherUserId);
 
-                      return StreamBuilder(
-                        stream: user,
-                        builder: (context, snapshot) {
-                          if (snapshot.connectionState ==
-                              ConnectionState.waiting) {
-                            return Padding(
-                              padding: const EdgeInsets.all(10.0),
-                              child: ListTile(
-                                shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(24),
-                                ),
-                                tileColor: Theme.of(context)
-                                    .colorScheme
-                                    .inversePrimary,
-                                title: Center(
-                                  child: CircularProgressIndicator(),
+                  return StreamBuilder(
+                    stream: user,
+                    builder: (context, snapshot) {
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return SizedBox.shrink();
+                      }
+
+                      if (!snapshot.hasData) {
+                        return Center(
+                          child: Text("Error loading user data"),
+                        );
+                      }
+
+                      final userData = snapshot.data!;
+                      String username = userData["username"];
+                      String imageUrl = userData["image_url"];
+
+                      if (username.contains(query) || query.isEmpty) {
+                        return MessageTile(
+                          username: username,
+                          imageUrl: imageUrl,
+                          userId: chatRoomId[0],
+                          otherUserId: chatRoomId[1],
+                          onOpenMessage: () {
+                            Navigator.of(context).push(
+                              MaterialPageRoute(
+                                builder: (context) => ChatScreen(
+                                  otherUserData: userData,
+                                  otherUserId: otherUserId,
                                 ),
                               ),
                             );
-                          }
-
-                          if (!snapshot.hasData) {
-                            return Center(
-                              child: Text("Error loading user data"),
-                            );
-                          }
-
-                          final userData = snapshot.data!;
-                          String username = userData["username"];
-                          String imageUrl = userData["image_url"];
-
-                          if (username.contains(query)) {
-                            return MessageTile(
-                              username: username,
-                              imageUrl: imageUrl,
-                              userId: chatRoomId[0],
-                              otherUserId: chatRoomId[1],
-                              onOpenMessage: () {
-                                Navigator.of(context).push(
-                                  MaterialPageRoute(
-                                    builder: (context) => ChatScreen(
-                                      otherUserData: userData,
-                                      otherUserId: otherUserId,
-                                    ),
-                                  ),
-                                );
-                              },
-                            );
-                          }
-                          return SizedBox.shrink();
-                        },
-                      );
+                          },
+                        );
+                      }
+                      return SizedBox.shrink();
                     },
-                  ),
-                ],
+                  );
+                },
               );
             },
           ),
