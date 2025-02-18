336a0d3be5019f99d88ef3c81890d09fd0b90eb0
Nguyễn Minh Đức <148557640+nmd1406@users.noreply.github.com>
2025-02-19 00:25:37 +0700

    add messageType field to determine message type

--- lib/widgets/message_bubble.dart
+++ lib/widgets/message_bubble.dart
@@ -1,3 +1,4 @@
+import 'package:chat_app/widgets/file_message_bubble.dart';
 import 'package:flutter/material.dart';
 
 // A MessageBubble for showing a single chat message on the ChatScreen.
@@ -8,6 +9,7 @@ class MessageBubble extends StatelessWidget {
     required this.userImage,
     required this.username,
     required this.message,
+    required this.messageType,
     required this.isMe,
   }) : isFirstInSequence = true;
 
@@ -15,6 +17,7 @@ class MessageBubble extends StatelessWidget {
   const MessageBubble.next({
     super.key,
     required this.message,
+    required this.messageType,
     required this.isMe,
   })  : isFirstInSequence = false,
         userImage = null,
@@ -35,6 +38,7 @@ class MessageBubble extends StatelessWidget {
   // Not required if the message is not the first in a sequence.
   final String? username;
   final String message;
+  final String messageType;
 
   // Controls how the MessageBubble will be aligned.
   final bool isMe;
@@ -89,53 +93,57 @@ class MessageBubble extends StatelessWidget {
                       ),
                     ),
 
-                  // The "speech" box surrounding the message.
-                  Container(
-                    decoration: BoxDecoration(
-                      color: isMe
-                          ? Colors.grey[300]
-                          : theme.colorScheme.secondary.withAlpha(200),
-                      // Only show the message bubble's "speaking edge" if first in
-                      // the chain.
-                      // Whether the "speaking edge" is on the left or right depends
-                      // on whether or not the message bubble is the current user.
-                      borderRadius: BorderRadius.only(
-                        topLeft: !isMe && isFirstInSequence
-                            ? Radius.zero
-                            : const Radius.circular(12),
-                        topRight: isMe && isFirstInSequence
-                            ? Radius.zero
-                            : const Radius.circular(12),
-                        bottomLeft: const Radius.circular(12),
-                        bottomRight: const Radius.circular(12),
-                      ),
+                  if (messageType == "text")
+                    // The "speech" box surrounding the message.
+                    Container(
+                        decoration: BoxDecoration(
+                          color: isMe
+                              ? Colors.grey[300]
+                              : theme.colorScheme.secondary.withAlpha(200),
+                          // Only show the message bubble's "speaking edge" if first in
+                          // the chain.
+                          // Whether the "speaking edge" is on the left or right depends
+                          // on whether or not the message bubble is the current user.
+                          borderRadius: BorderRadius.only(
+                            topLeft: !isMe && isFirstInSequence
+                                ? Radius.zero
+                                : const Radius.circular(12),
+                            topRight: isMe && isFirstInSequence
+                                ? Radius.zero
+                                : const Radius.circular(12),
+                            bottomLeft: const Radius.circular(12),
+                            bottomRight: const Radius.circular(12),
+                          ),
+                        ),
+                        // Set some reasonable constraints on the width of the
+                        // message bubble so it can adjust to the amount of text
+                        // it should show.
+                        constraints: const BoxConstraints(maxWidth: 200),
+                        padding: const EdgeInsets.symmetric(
+                          vertical: 10,
+                          horizontal: 14,
+                        ),
+                        // Margin around the bubble.
+                        margin: const EdgeInsets.symmetric(
+                          vertical: 4,
+                          horizontal: 12,
+                        ),
+                        child: Text(
+                          message,
+                          style: TextStyle(
+                            // Add a little line spacing to make the text look nicer
+                            // when multilined.
+                            height: 1.3,
+                            color: isMe
+                                ? Colors.black87
+                                : theme.colorScheme.onSecondary,
+                          ),
+                          softWrap: true,
+                        ))
+                  else
+                    FileMessageBubble(
+                      userImage: message,
                     ),
-                    // Set some reasonable constraints on the width of the
-                    // message bubble so it can adjust to the amount of text
-                    // it should show.
-                    constraints: const BoxConstraints(maxWidth: 200),
-                    padding: const EdgeInsets.symmetric(
-                      vertical: 10,
-                      horizontal: 14,
-                    ),
-                    // Margin around the bubble.
-                    margin: const EdgeInsets.symmetric(
-                      vertical: 4,
-                      horizontal: 12,
-                    ),
-                    child: Text(
-                      message,
-                      style: TextStyle(
-                        // Add a little line spacing to make the text look nicer
-                        // when multilined.
-                        height: 1.3,
-                        color: isMe
-                            ? Colors.black87
-                            : theme.colorScheme.onSecondary,
-                      ),
-                      softWrap: true,
-                    ),
-                  ),
                 ],
               ),
             ],
