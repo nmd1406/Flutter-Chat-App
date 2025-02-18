c6778240702cc36b3696506afefb8f446a87783e
Nguyễn Minh Đức <148557640+nmd1406@users.noreply.github.com>
2025-02-19 00:23:50 +0700

    fix getAvatarUrl() and add uploadFiles()

--- lib/services/storage_service.dart
+++ lib/services/storage_service.dart
@@ -1,8 +1,9 @@
 import 'dart:io';
 
-import 'package:chat_app/services/auth_service.dart';
+import 'package:path/path.dart' as path;
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
+import 'package:chat_app/services/auth_service.dart';
 
 final _authService = AuthService();
 
@@ -11,7 +12,7 @@ class StorageService {
   static final _firestore = FirebaseFirestore.instance;
 
   Future<String> getAvatarUrl(String uid) async {
-    final storageRef = _storage.ref().child("user_images").child(uid);
+    final storageRef = _storage.ref().child("user_images").child("$uid.jpg");
 
     return await storageRef.getDownloadURL();
   }
@@ -27,5 +28,15 @@ class StorageService {
         .collection("users")
         .doc(uid)
         .update({"image_url": imageUrl});
+  }
+
+  Future<String> uploadFiles(File file) async {
+    String filePath = file.path;
+    String fileName = path.basename(filePath);
+
+    var storageRef = _storage.ref().child("media_files").child(fileName);
+    await storageRef.putFile(file);
+
+    return await storageRef.getDownloadURL();
   }
 }
