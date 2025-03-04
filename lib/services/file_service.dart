fcc011e51dc7ab009a5386b8bd9bd5a8f284ce93
Nguyễn Minh Đức <148557640+nmd1406@users.noreply.github.com>
2025-03-04 23:47:10 +0700

    add functions to get video/image dimension (width x height). now openFile function will handle ResultType.noAppToOpen when open a file

--- lib/services/file_service.dart
+++ lib/services/file_service.dart
@@ -1,10 +1,15 @@
+import 'dart:async';
 import 'dart:io';
 import 'dart:math';
+import 'dart:ui' as ui;
 
+import 'package:flutter/material.dart';
+import 'package:cached_video_player_plus/cached_video_player_plus.dart';
 import 'package:dio/dio.dart';
 import 'package:open_file/open_file.dart';
 import 'package:path_provider/path_provider.dart' as path;
 import 'package:permission_handler/permission_handler.dart';
+import 'package:url_launcher/url_launcher.dart';
 import 'package:http/http.dart' as http;
 
 class FileService {
@@ -12,7 +17,8 @@ class FileService {
     return await Permission.manageExternalStorage.request().isGranted;
   }
 
-  Future<File?> openFile(String fileUrl, String fileName) async {
+  Future<File?> openFile(
+      String fileUrl, String fileName, BuildContext context) async {
     // Then in your openFile method:
     if (!await requestPermissions()) {
       print("Storage permission denied");
@@ -28,7 +34,12 @@ class FileService {
       // Check if file already exists
       if (await file.exists()) {
         print("File already exists, opening...");
-        OpenFile.open(filePath);
+        var result = await OpenFile.open(filePath);
+        if (result.type == ResultType.noAppToOpen && context.mounted) {
+          // Show dialog to suggest installing an app
+          _showNoAppDialog(context);
+        }
+
         return file;
       }
 
@@ -43,17 +54,94 @@ class FileService {
       });
 
       print("File downloaded to: $filePath");
-      OpenFile.open(filePath);
+      await OpenFile.open(filePath);
       return file;
     } catch (e) {
       print("Error: $e");
     }
+    return null;
+  }
+
+  void _showNoAppDialog(BuildContext context) {
+    // Show a dialog prompting the user to install a DOCX viewer
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text("Không mở được file"),
+        content: Text(
+            "Thiết bị không có ứng dụng hỗ trợ mở file này. Vui lòng tìm ứng dụng hỗ trợ file trên Play Store."),
+        actions: [
+          ElevatedButton(
+            onPressed: () async {
+              await launchUrl(
+                Uri.parse("market://details?id=com.android.vending"),
+                mode: LaunchMode.externalApplication,
+              );
+            },
+            child: Text("Mở Play Store"),
+          ),
+          TextButton(
+            onPressed: () => Navigator.pop(context),
+            child: Text("Đóng"),
+          ),
+        ],
+      ),
+    );
   }
 
   Future<bool> isFileExist(String fileName) async {
     Directory directory = await path.getApplicationDocumentsDirectory();
     File file = File('${directory.path}/media_files/$fileName');
     return await file.exists();
+  }
+
+  Future<Size> getImageDimensionFromFile(File file) async {
+    final Completer<Size> completer = Completer<Size>();
+
+    ui.decodeImageFromList(file.readAsBytesSync(), (image) {
+      completer.complete(Size(image.width.toDouble(), image.height.toDouble()));
+    });
+
+    return completer.future;
+  }
+
+  Future<Size> getImageDimensionFromUrl(String imageUrl) async {
+    final Completer<Size> completer = Completer();
+    final Image image = Image.network(imageUrl);
+
+    image.image.resolve(const ImageConfiguration()).addListener(
+      ImageStreamListener((ImageInfo imageInfo, bool synchronousCall) {
+        final Size size = Size(
+          imageInfo.image.width.toDouble(),
+          imageInfo.image.height.toDouble(),
+        );
+        completer.complete(size);
+      }),
+    );
+
+    return completer.future;
+  }
+
+  Future<Size> getVideoDimensionFromFile(File file) async {
+    final CachedVideoPlayerPlusController controller =
+        CachedVideoPlayerPlusController.file(file);
+
+    await controller.initialize(); // Ensure video is fully initialized
+    return Size(
+      controller.value.size.width,
+      controller.value.size.height,
+    );
+  }
+
+  Future<Size> getVideoDimensionFromUrl(String videoUrl) async {
+    final CachedVideoPlayerPlusController controller =
+        CachedVideoPlayerPlusController.networkUrl(Uri.parse(videoUrl));
+
+    await controller.initialize(); // Wait until the video loads its metadata
+    return Size(
+      controller.value.size.width,
+      controller.value.size.height,
+    );
   }
 
   Map<String, String> getFileInfoFromUrl(String downloadUrl) {
@@ -66,7 +154,7 @@ class FileService {
       String fileName = uri.pathSegments.last;
 
       // Decode the URL-encoded file name
-      fileName = Uri.decodeFull(fileName);
+      fileName = Uri.decodeFull(fileName).split("media_files/")[1];
 
       // Get file extension
       String fileExtension = '';
